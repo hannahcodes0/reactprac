ad807e09cce43608bbdbbb273d701335c8e0a803
hannahcodes0 <hannahhayoungkim2@gmail.com>
2022-11-28 18:03:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import CustomButton from "./components/CustomButton";
+import User from "./components/User";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: "송중기" },
+    { id: 2, age: 24, name: "송강" },
+    { id: 3, age: 21, name: "김유정" },
+    { id: 4, age: 29, name: "구교환" },
+  ]);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      age: age,
+      name: name,
+    };
+
+    setUsers([...users, newUser]);
+  };
+
+  const deleteUserHandler = (id) => {
+    const newUserList = users.filter((user) => user.id !== id);
+    setUsers(newUserList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app-style">
+        {users.map((user) => {
+          if (user.age < 25) {
+            return (
+              <User
+                handleDelete={deleteUserHandler}
+                user={user}
+                key={user.id}
+              ></User>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <input
+        value={name}
+        placeholder="이름을 입력해주세요"
+        // 인풋 이벤트로 들어온 입력 값을 name의 값으로 업데이트
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        value={age}
+        placeholder="나이를 입력해주세요"
+        // 인풋 이벤트로 들어온 입력 값을 age의 값으로 업데이트
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <CustomButton color="green" onClick={addUserHandler}>
+        추가하기
+      </CustomButton>
     </div>
   );
-}
+};
 
 export default App;
